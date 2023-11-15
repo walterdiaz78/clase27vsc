e0fa99c685e2220bcc156b52cf12c009ac6904ec
Walter Diaz <32580430+walterdiaz78@users.noreply.github.com>
2023-11-15 04:50:17 +0000

    Minuto 48:00 de clase 27

--- database/generador_users.py
+++ database/generador_users.py
@@ -12,21 +12,41 @@ dni:list = [random.randint(6_000_000, 50_000_000) for in range(100)]
 
 
 def generate_german_email(name):
+    """
+    Genera una dirección de correo electrónico aleatoria con un nombre alemán y un dominio predeterminado.
+
+    Parameters:
+    - name (str): El nombre que se utilizará para crear la dirección de correo electrónico.
+
+    Returns:
+    str: Una cadena que representa la dirección de correo electrónico generada.
+
+    Example:
+    >>> generate_german_email("Max")
+    'max@gmail.com'
+    """
     domains=["gmail.com", "yahoo.com", "outlook.com"]
     random_domain = random.choice(domains)
     return f"{name.lower()}@{random_name}.{random_domain}"
 
 def create_user(nombre: str = None, email: str = None, dni: int = None) -> (dict):
+    """
+    Crea un diccionario representando un usuario con información opcional.
+
+    Parameters:
+    - nombre (str, opcional): El nombre del usuario. Si no se proporciona, se elige uno aleatorio.
+    - email (str, opcional): La dirección de correo electrónico del usuario. Si no se proporciona, se elige una aleatoria.
+    - dni (int, opcional): El número de identificación del usuario. Si no se proporciona, se elige uno aleatorio.
+
+    Returns:
+    dict: Un diccionario que representa la información del usuario con claves "nombre", "email" y "dni".
+    """
     if nombre is None and email is None and dni is None:
         return {
             "nombre": random.choice(names)
             "email": random.choice(emails)
             "dni": random.choice(dni)
-            
-
         }
-        
-
        
     return {
         "nombre": nombre,
@@ -35,6 +55,15 @@ def create_user(nombre: str = None, email: str = None, dni: int = None) -> (dict
     }
 
 def create-database(cant_user:int=10):
+    """
+    Crea una base de datos simulada con usuarios.
+
+    Parameters:
+    - cant_user (int, opcional): La cantidad de usuarios a incluir en la base de datos. Por defecto, se generan 10 usuarios.
+
+    Returns:
+    list: Una lista que representa la base de datos simulada, con una cantidad de usuarios igual a `cant_user`.
+    """
     return [
         for _ in range(cant_user)
     ]
--- main.py
+++ main.py
@@ -3,9 +3,24 @@
 
 def greeting(nombre:str) -> None:
     print(f"Hola, {nombre} 😎")
+    return None #si no retorna nada es un procedimiento
 
+#asigno input a variabkle
 nombre= input("Ingresa tu nombre:\n")
 
 #llamar a la funcion
 greeting(nombre)
-#greeting(nombre:= input("Ingresa tu nombre: \n"))
+#greeting(nombre:= input("Ingresa tu nombre: \n")) #operador wallroute o algo asi
+
+#CRUD, create, read, upload, delete
+"""
+database (crear un usuario | muchos usuarios)
+- ingreso la operacion del crud
+- controllers: logica del negocio
+- que paso hacer despues
+"""
+ from database.generador_users import create_database
+
+ db_user:list[dict] = create_database()
+
+print(db_user)

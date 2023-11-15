a3646c3731cd128f443b8e07f492aa198c41775f
Walter Diaz <32580430+walterdiaz78@users.noreply.github.com>
2023-11-15 03:09:37 +0000

    primer commit clase 27 funciones python - entorno virtual

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+#FUNCIONES
+#---------------------------------------------------------
 
+def greeting(nombre:str) -> None:
+    print(f"Hola, {nombre} 😎")
+
+nombre= input("Ingresa tu nombre:\n")
+
+#llamar a la funcion
+greeting(nombre)
+#greeting(nombre:= input("Ingresa tu nombre: \n"))
